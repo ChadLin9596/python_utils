6640bbaa9663c040520c3c5e82c8e924dbe6e82b
Chad Lin <chun-jung.lin@adelaide.edu.au>
2025-02-17 16:10:12 +1030

    [WIP] added a `segmented_where` to help tracing max & min indices for segmented_max|min

--- src/utils_segmentation.py
+++ src/utils_segmentation.py
@@ -110,9 +110,35 @@ def segmented_mean(x, s_ind, e_ind):
     return 1.0 * r / c.reshape(shp)
 
 
-def segmented_max(x, s_ind, e_ind):
+def segmented_where(x, s_ind, e_ind, values):
+    """
+    TODO: add docstring
+    """
+    x = np.asarray(x)
+    s_ind = np.asarray(s_ind)
+    e_ind = np.asarray(e_ind)
+    values = np.asarray(values)
+
+    if s_ind.shape != e_ind.shape or s_ind.ndim != 1:
+        raise ValueError(
+            "s_ind and e_ind must be 1D arrays of the same shape."
+        )
+
+    results = []
+
+    for s, e, val in zip(s_ind, e_ind, values):
+        result = np.where(x[s:e] == val)[0]
+        results.append(result)
+
+    return results
+
+
+def segmented_max(x, s_ind, e_ind, return_indices=False):
     """
     Computes the maximum along segments x[s_ind:e_ind, ...]
+    Returns:
+        - max values within each segment
+        - (optional) indices of these max values in the original array
     """
     x = np.array(x)
     s_ind = np.array(s_ind)
@@ -134,12 +160,21 @@ def segmented_max(x, s_ind, e_ind):
 
     x = np.concatenate([x, x[-1:]], axis=0)
     splits = np.vstack([s_ind, e_ind]).T.flatten()
-    return np.maximum.reduceat(x, splits)[::2]
+
+    results = np.maximum.reduceat(x, splits)[::2]
+
+    if return_indices:
+        max_ind = segmented_where(x, s_ind, e_ind, results)
+        return results, max_ind
+    return results
 
 
-def segmented_min(x, s_ind, e_ind):
+def segmented_min(x, s_ind, e_ind, return_indices=False):
     """
     Computes the minimum along segments x[s_ind:e_ind, ...]
+    Returns:
+        - min values within each segment
+        - (optional) indices of these min values in the original array
     """
     x = np.array(x)
     s_ind = np.array(s_ind)
@@ -161,4 +196,10 @@ def segmented_min(x, s_ind, e_ind):
 
     x = np.concatenate([x, x[-1:]], axis=0)
     splits = np.vstack([s_ind, e_ind]).T.flatten()
-    return np.minimum.reduceat(x, splits)[::2]
+
+    results = np.minimum.reduceat(x, splits)[::2]
+
+    if return_indices:
+        min_ind = segmented_where(x, s_ind, e_ind, results)
+        return results, min_ind
+    return results
