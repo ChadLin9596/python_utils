649bcf3fd92587e50fa27e60b9b0b74339924909
Chad Lin <chun-jung.lin@adelaide.edu.au>
2025-09-01 15:28:20 +0930

    wrapped project using pyproject.toml (PEP 621) specified python version >=3.7 by testing

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension
 import numpy as np
-
-description = """
-A few utility functions for Robotics, Computer Vision, Machine Learning
-"""
 
 ext_modules = [
     Extension(
@@ -19,26 +15,5 @@ ext_modules = [
 ]
 
 setup(
-    name="py_utils",
-    version="0.1.0",
-    description=description,
-    author="Chad Lin",
-    url="https://github.com/ChadLin9596/python_utils",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
     ext_modules=ext_modules,
-    install_requires=[
-        "dill",
-        "lzf",
-        "matplotlib",
-        "numpy<2",
-        "opencv-python<4.12.0.88",
-        "pandas",
-        "Pillow",
-        "requests",
-        "scipy",
-        "scikit-image",
-        "torch",
-        "torchvision",
-    ],
 )
